da409bc58132ebe2abe3b0096e456612347f48ec
mifumi323 <mifumi323@gmail.com>
2024-12-11 23:15:08 +0900

    Misc.bas相当のコード

---
+++ funya1_wpf/Misc.cs
@@ -0,0 +1,21 @@
+﻿namespace funya1_wpf
+{
+    public class Misc
+    {
+        private int C = 0;
+
+        public void Change123(ref int x, int Min, int Max)
+        {
+            x++;
+            if (x > Max) x = Min;
+        }
+
+        public void Change12321(ref int x, int Min, int Max)
+        {
+            x += C;
+            if (C == 1 && x >= Max) C = -1;
+            if (C == 0) C = 1;
+            if (C == -1 && x <= Min) C = 1;
+        }
+    }
+}
