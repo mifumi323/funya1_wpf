f8d7cad46eac7997d0d0e2d252ddfde566f66e38
mifumi323 <mifumi323@gmail.com>
2024-12-22 21:12:49 +0900

    音楽ファイル参照

--- funya1_wpf/FormMusic.xaml.cs
+++ funya1_wpf/FormMusic.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace funya1_wpf
 {
@@ -16,6 +18,21 @@ namespace funya1_wpf
         {
             DialogResult = true;
             Close();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var OpenButton = (Button)sender;
+            var dialog = new OpenFileDialog()
+            {
+                Filter = "音楽ファイル|*.mid;*.mp3|全てのファイル|*.*",
+                Title = "音楽ファイルを開く",
+                FileName = OpenButton.Tag as string,
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                OpenButton.Tag = dialog.FileName;
+            }
         }
     }
 }
