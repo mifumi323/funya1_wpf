272d149d51531c05fab9a433791a1828d0196606
mifumi323 <mifumi323@gmail.com>
2024-12-26 23:16:16 +0900

    ActionCommandクラスの追加と名前空間の整理
    - `System.Windows.Input`名前空間の使用を追加
    - `funya1_wpf`名前空間を新たに追加
    - `ActionCommand`クラスを追加 (`ICommand`を実装)
      - `Execute`メソッドは`Action<object?>`デリゲートを使用
      - `CanExecute`メソッドは常に`true`を返す
      - `CanExecuteChanged`イベントを宣言 (未実装)

---
+++ funya1_wpf/ActionCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace funya1_wpf
+{
+    public class ActionCommand(Action<object?> action) : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter) => action(parameter);
+    }
+}
